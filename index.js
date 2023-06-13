4e69da317447d6d7014be1f7c11fe4f4a4376da3
R7558 <rupsanaworld1004@gmail.co>
2023-06-14 01:51:05 +0600

    array part

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //get started....
 var userName = 'Rupsana Akter';
-console.log(userName);
+// console.log(userName);
 var num = 546858;
-console.log(num);
+// console.log(num);
 // simple types...
 var firstName;
 var roll;
@@ -11,3 +11,13 @@ firstName = 'Aman';
 roll = 75465;
 position = true;
 console.log("My name is ".concat(firstName, " and roll ").concat(roll, ". I have a Position ").concat(position));
+// special types.....
+var value;
+value = 'Rup';
+value = 45546;
+value = true;
+console.log(value);
+//array .......
+var arr = ['jibon'];
+arr.push('aman');
+console.log(arr);
--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 //get started....
 let userName='Rupsana Akter';
-console.log(userName);
+// console.log(userName);
 let num=546858;
-console.log(num);
+// console.log(num);
 // simple types...
 let firstName:string;
 let roll:number;
@@ -12,3 +12,13 @@ roll=75465;
 position=true;
 console.log(`My name is ${firstName} and roll ${roll}. I have a Position ${position}`);
 
+// special types.....
+let value:any;
+value='Rup'
+value=45546;
+value=true
+console.log(value);
+//array .......
+const arr:string[]=['jibon'];
+arr.push('aman');
+console.log(arr);
