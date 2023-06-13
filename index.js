3c27274f4d41bdebcf184ee3b412ddc2690b12d6
R7558 <rupsanaworld1004@gmail.co>
2023-06-14 01:41:40 +0600

    added update

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+//get started....
 var userName = 'Rupsana Akter';
 console.log(userName);
 var num = 546858;
 console.log(num);
+// simple types...
+var firstName;
+var roll;
+var position;
+firstName = 'Aman';
+roll = 75465;
+position = true;
+console.log("My name is ".concat(firstName, " and roll ").concat(roll, ". I have a Position ").concat(position));
--- index.ts
+++ index.ts
@@ -1,4 +1,14 @@
+//get started....
 let userName='Rupsana Akter';
 console.log(userName);
 let num=546858;
 console.log(num);
+// simple types...
+let firstName:string;
+let roll:number;
+let position:boolean;
+firstName='Aman';
+roll=75465;
+position=true;
+console.log(`My name is ${firstName} and roll ${roll}. I have a Position ${position}`);
+
