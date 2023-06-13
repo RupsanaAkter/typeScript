24894342e23e8c9b4838af66b67f2a9a7d93dbca
R7558 <rupsanaworld1004@gmail.co>
2023-06-14 02:17:43 +0600

    added union

--- index.js
+++ index.js
@@ -1,43 +1,60 @@
-//get started....
-var userName = 'Rupsana Akter';
-// console.log(userName);
-var num = 546858;
-// console.log(num);
-// simple types...
-var firstName;
-var roll;
-var position;
-firstName = 'Aman';
-roll = 75465;
-position = true;
-console.log("My name is ".concat(firstName, " and roll ").concat(roll, ". I have a Position ").concat(position));
-// special types.....
-var value;
-value = 'Rup';
-value = 45546;
-value = true;
-console.log(value);
-//array .......
-var arr = ['jibon'];
-arr.push('aman');
-console.log(arr);
-var numbers = [1, 2, 3];
-numbers.push(4);
-numbers.push(67);
-console.log(numbers);
-//Tuples.........
-var ourTuple;
-ourTuple = [56, false, 'Badol'];
-console.log(ourTuple);
-var output;
-output = [45, true, 'Vir'];
-output.push(34);
-output.push('Belel');
-console.log(output);
-//object types.......
+// //get started....
+// let userName='Rupsana Akter';
+// // console.log(userName);
+// let num=546858;
+// // console.log(num);
+// // simple types...
+// let firstName:string;
+// let roll:number;
+// let position:boolean;
+// firstName='Aman';
+// roll=75465;
+// position=true;
+// console.log(`My name is ${firstName} and roll ${roll}. I have a Position ${position}`);
+// // special types.....
+// let value:any;
+// value='Rup'
+// value=45546;
+// value=true
+// console.log(value);
+// //array .......
+// const arr:string[]=['jibon'];
+// arr.push('aman');
+// console.log(arr);
+// const numbers = [1, 2, 3]; 
+// numbers.push(4);
+// numbers.push(67)
+// console.log(numbers);
+// //Tuples.........
+// let ourTuple: [number, boolean, string];
+// ourTuple=[56,false,'Badol']
+// console.log(ourTuple);
+// let output:[number,boolean,string];
+// output=[45,true,'Vir'];
+// output.push(34);
+// output.push('Belel')
+// console.log(output);
+// //object types.......
+// const car: { type: string, model: string, year: number } = {
+//     type: "Toyota",
+//     model: "Corolla",
+//     year: 2009
+//   };
+//   console.log(car);
 var car = {
-    type: "Toyota",
-    model: "Corolla",
-    year: 2009
+    type: 'BMW',
+    model: 'ryt',
+    year: 567
 };
 console.log(car);
+var cars = {
+    type: "Toyota",
+};
+cars.type = "Ford";
+console.log(cars);
+//union types......
+function printStatusCode(code) {
+    console.log("My status code is ".concat(code, "."));
+}
+printStatusCode(404);
+printStatusCode('404');
--- index.ts
+++ index.ts
@@ -1,58 +1,81 @@
-//get started....
-let userName='Rupsana Akter';
-// console.log(userName);
+// //get started....
+// let userName='Rupsana Akter';
+// // console.log(userName);
 
 
-let num=546858;
-// console.log(num);
+// let num=546858;
+// // console.log(num);
 
 
-// simple types...
-let firstName:string;
-let roll:number;
-let position:boolean;
-firstName='Aman';
-roll=75465;
-position=true;
-console.log(`My name is ${firstName} and roll ${roll}. I have a Position ${position}`);
+// // simple types...
+// let firstName:string;
+// let roll:number;
+// let position:boolean;
+// firstName='Aman';
+// roll=75465;
+// position=true;
+// console.log(`My name is ${firstName} and roll ${roll}. I have a Position ${position}`);
 
 
-// special types.....
-let value:any;
-value='Rup'
-value=45546;
-value=true
-console.log(value);
+// // special types.....
+// let value:any;
+// value='Rup'
+// value=45546;
+// value=true
+// console.log(value);
 
 
-//array .......
-const arr:string[]=['jibon'];
-arr.push('aman');
-console.log(arr);
+// //array .......
+// const arr:string[]=['jibon'];
+// arr.push('aman');
+// console.log(arr);
 
 
-const numbers = [1, 2, 3]; 
-numbers.push(4);
-numbers.push(67)
-console.log(numbers);
+// const numbers = [1, 2, 3]; 
+// numbers.push(4);
+// numbers.push(67)
+// console.log(numbers);
 
 
-//Tuples.........
-let ourTuple: [number, boolean, string];
-ourTuple=[56,false,'Badol']
-console.log(ourTuple);
+// //Tuples.........
+// let ourTuple: [number, boolean, string];
+// ourTuple=[56,false,'Badol']
+// console.log(ourTuple);
 
-let output:[number,boolean,string];
-output=[45,true,'Vir'];
-output.push(34);
-output.push('Belel')
-console.log(output);
+// let output:[number,boolean,string];
+// output=[45,true,'Vir'];
+// output.push(34);
+// output.push('Belel')
+// console.log(output);
 
-//object types.......
-const car: { type: string, model: string, year: number } = {
-    type: "Toyota",
-    model: "Corolla",
-    year: 2009
-  };
+// //object types.......
+// const car: { type: string, model: string, year: number } = {
+//     type: "Toyota",
+//     model: "Corolla",
+//     year: 2009
+//   };
   
-  console.log(car);
+//   console.log(car);
+
+let car:{type:string,model:string,year:number}={
+    type:'BMW',
+    model:'ryt',
+    year:567
+}
+console.log(car);
+
+
+const cars = {
+    type: "Toyota",
+  };
+  cars.type = "Ford"; 
+   console.log(cars);
+
+   //union types......
+
+   function printStatusCode(code: string | number) {
+    console.log(`My status code is ${code}.`)
+  }
+  
+  printStatusCode(404);
+  printStatusCode('404');
